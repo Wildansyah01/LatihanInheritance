8fd1d8dd527912159a62fec79651d3460c71c010
Wildansyah01 <101808641+Wildansyah01@users.noreply.github.com>
2022-06-09 15:26:28 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", 29);
+            // person.Name = "Mike";
+            // person.Age = 299;
+            person.GetNameAndAge();
+
+            Teacher tch = new Teacher("Thomas", 22, "209148", "Math");
+            // tch.Name = "Thomas";
+            // tch.Age = 22;
+            // tch.TeacherId = "809328";
+            // tch.Subject = "Math";
+            tch.GetNameAndAge();
+
+            Student std = new Student("Sara", 19, "901341", "augag@gmail.com");
+            // std.Name = "Sara";
+            // std.Age = 19;
+            // std.StudentId = "29035092";
+            // std.Email = "asogh@gmail.com";
+            std.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
